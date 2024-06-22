5dfd90e5ef5bbf4e395ecb513888a7ffa8923e38
hansen1416 <badapplesweetie@gmail.com>
2024-06-22 18:35:05 +0800

    1

--- src/app.py
+++ src/app.py
@@ -14,3 +14,12 @@ CORS(app)
 app.register_blueprint(HomeController)
 app.register_blueprint(CategoryController)
 app.register_blueprint(GalleryController)
+
+"""
+1. 作者的描述，越多越好，尽量带作者的派别
+2. 作品的题材，流派信息，用作分类
+3. 作品的赏析
+4. 画作名字的搜索，年代搜索，（尽量模糊）
+5. 作品的相关推荐
+6. 多语言
+"""
