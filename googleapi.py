d7eb760d336747688875a347e3ef7c4ebe77ac8a
hansen1416 <badapplesweetie@gmail.com>
2024-06-30 16:46:44 +0800

    1

--- googleapi.py
+++ googleapi.py
@@ -12,16 +12,19 @@ model = genai.GenerativeModel("gemini-1.5-flash")
 
 database = Database()
 
-query = "select attribution from attributions order by attributionid asc;"
+query = "select attribution, attribution from attributions order by attributionid asc;"
 
 data = database.query(query)
 
 for name in data:
-    print(name[0], type(name[0]))
+    print(name[1], type(name[1]))
 
     response = model.generate_content(
-        f"Give me a description of {name[0]}, answer only contains the description itself, no special charasters, not more than 2000 charasters."
+        f"Give me a description of {name[1]}, answer only contains the description itself, no special charasters, not more than 2000 charasters."
     )
     print(response.text)
 
     break
+
+    # save the response to the database, column `description` of table `attributions`
+    query = f"update attributions set description = '{response.text}' where attribution = '{name[0]}';"
