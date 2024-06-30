092ea85a712231d4e846d3ce44a779b524c7b882
hansen1416 <badapplesweetie@gmail.com>
2024-06-30 17:10:47 +0800

    1

--- googleapi.py
+++ googleapi.py
@@ -1,6 +1,7 @@
 import google.generativeai as genai
 import os
 import dotenv
+import time
 
 from src.services.Database import Database
 
@@ -17,7 +18,6 @@ query = "select attribution, attribution from attributions where description is 
 data = database.query(query)
 
 for name in data:
-    print(name[1], type(name[1]))
 
     response = model.generate_content(
         f"Give me a description of {name[1]}, answer only contains the description itself, no special charasters, not more than 2000 charasters."
@@ -31,6 +31,11 @@ for name in data:
     # save the response to the database, column `description` of table `attributions`
     query = f"update attributions set description = E'{description}' where attribution = '{name[0]}';"
 
-    print(f"description saved for {name[0]}, {name[1]}.")
+    print(
+        f"description saved for {name[0]}, {name[1]}, description length {len(description)}."
+    )
 
     database.execute(query)
+
+    # Pause execution for 2 seconds
+    time.sleep(2)
